37c5c2b139e7d4b43fc157f90fd5f67ab49ec7da
johnswaroop <71548636+johnswaroop@users.noreply.github.com>
2022-10-17 13:58:24 +0530

    init

---
+++ pages/resource/index.jsx
@@ -0,0 +1,118 @@
+import React from 'react'
+import styles from './chain.module.scss'
+
+function index() {
+    return (
+        <div className={styles.chainPage}>
+            <div className={styles.head}>
+                <img src="./polygonIcon.png" alt="" />
+                <div className={styles.info}>
+                    <h1>Polygon</h1>
+                    <h2>About Polygon</h2>
+                    <p>This JavaScript crash course covers the basics of programming through advanced topics like asynchronous requests and promises. The most used developer tools and libraries for Ethereum are JavaScript based, making it a key foundation to web3 development.</p>
+                </div>
+            </div>
+            <div className={styles.resourcesCon}>
+
+                <div className={styles.resource}>
+                    <img src="./polygonIcon.png" alt="" />
+                    <div className={styles.rInfo}>
+                        <h1>Course Name</h1>
+                        <div className={styles.tags}>
+                            <span>Product Designer</span>
+                            <span>Product Designer</span>
+                        </div>
+                        <p>Polygon is an Indian blockchain scalability platform. It addresses the challenges faced by Ethereum. Polygon is an Indian blockchain scalability platform. It addresses the challenges faced by Ethereum.</p>
+                        <button>Go to the Course</button>
+                    </div>
+                </div>
+                <div className={styles.resource}>
+                    <img src="./polygonIcon.png" alt="" />
+                    <div className={styles.rInfo}>
+                        <h1>Course Name</h1>
+                        <div className={styles.tags}>
+                            <span>Product Designer</span>
+                            <span>Product Designer</span>
+                        </div>
+                        <p>Polygon is an Indian blockchain scalability platform. It addresses the challenges faced by Ethereum. Polygon is an Indian blockchain scalability platform. It addresses the challenges faced by Ethereum.</p>
+                        <button>Go to the Course</button>
+                    </div>
+                </div>
+                <div className={styles.resource}>
+                    <img src="./polygonIcon.png" alt="" />
+                    <div className={styles.rInfo}>
+                        <h1>Course Name</h1>
+                        <div className={styles.tags}>
+                            <span>Product Designer</span>
+                            <span>Product Designer</span>
+                        </div>
+                        <p>Polygon is an Indian blockchain scalability platform. It addresses the challenges faced by Ethereum. Polygon is an Indian blockchain scalability platform. It addresses the challenges faced by Ethereum.</p>
+                        <button>Go to the Course</button>
+                    </div>
+                </div>
+                <div className={styles.resource}>
+                    <img src="./polygonIcon.png" alt="" />
+                    <div className={styles.rInfo}>
+                        <h1>Course Name</h1>
+                        <div className={styles.tags}>
+                            <span>Product Designer</span>
+                            <span>Product Designer</span>
+                        </div>
+                        <p>Polygon is an Indian blockchain scalability platform. It addresses the challenges faced by Ethereum. Polygon is an Indian blockchain scalability platform. It addresses the challenges faced by Ethereum.</p>
+                        <button>Go to the Course</button>
+                    </div>
+                </div>
+                <div className={styles.resource}>
+                    <img src="./polygonIcon.png" alt="" />
+                    <div className={styles.rInfo}>
+                        <h1>Course Name</h1>
+                        <div className={styles.tags}>
+                            <span>Product Designer</span>
+                            <span>Product Designer</span>
+                        </div>
+                        <p>Polygon is an Indian blockchain scalability platform. It addresses the challenges faced by Ethereum. Polygon is an Indian blockchain scalability platform. It addresses the challenges faced by Ethereum.</p>
+                        <button>Go to the Course</button>
+                    </div>
+                </div>
+                <div className={styles.resource}>
+                    <img src="./polygonIcon.png" alt="" />
+                    <div className={styles.rInfo}>
+                        <h1>Course Name</h1>
+                        <div className={styles.tags}>
+                            <span>Product Designer</span>
+                            <span>Product Designer</span>
+                        </div>
+                        <p>Polygon is an Indian blockchain scalability platform. It addresses the challenges faced by Ethereum. Polygon is an Indian blockchain scalability platform. It addresses the challenges faced by Ethereum.</p>
+                        <button>Go to the Course</button>
+                    </div>
+                </div>
+                <div className={styles.resource}>
+                    <img src="./polygonIcon.png" alt="" />
+                    <div className={styles.rInfo}>
+                        <h1>Course Name</h1>
+                        <div className={styles.tags}>
+                            <span>Product Designer</span>
+                            <span>Product Designer</span>
+                        </div>
+                        <p>Polygon is an Indian blockchain scalability platform. It addresses the challenges faced by Ethereum. Polygon is an Indian blockchain scalability platform. It addresses the challenges faced by Ethereum.</p>
+                        <button>Go to the Course</button>
+                    </div>
+                </div>
+                <div className={styles.resource}>
+                    <img src="./polygonIcon.png" alt="" />
+                    <div className={styles.rInfo}>
+                        <h1>Course Name</h1>
+                        <div className={styles.tags}>
+                            <span>Product Designer</span>
+                            <span>Product Designer</span>
+                        </div>
+                        <p>Polygon is an Indian blockchain scalability platform. It addresses the challenges faced by Ethereum. Polygon is an Indian blockchain scalability platform. It addresses the challenges faced by Ethereum.</p>
+                        <button>Go to the Course</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default index 
